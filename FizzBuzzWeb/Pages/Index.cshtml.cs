77b3a6e128e070a0239f6977333fc17b05bd6e0a
matik802 <mati88846@gmail.com>
2023-03-14 11:36:01 +0100

    zadanie 2

--- FizzBuzzWeb/Pages/Index.cshtml.cs
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FizzBuzzWeb.Forms;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FizzBuzzWeb.Pages
 {
+    [BindProperties]
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        public FizzBuzzForm fizzBuzz { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,7 +20,19 @@ namespace FizzBuzzWeb.Pages
 
         public void OnGet()
         {
+            fizzBuzz = new FizzBuzzForm();
+            if((string.IsNullOrWhiteSpace(Name))) Name = "User";
+        }
 
+        public void OnPost() 
+        {
+            if(ModelState.IsValid)
+            {
+                if (fizzBuzz.Number % 15 == 0) ViewData["Info"] = "FizzBuzz";
+                else if (fizzBuzz.Number % 3 == 0) ViewData["Info"] = "Fizz";
+                else if (fizzBuzz.Number % 5 == 0) ViewData["Info"] = "Buzz";
+                else ViewData["Info"] = "Liczba: "+fizzBuzz.Number+" nie spelnia kryteriow";
+            }
         }
     }
 }
